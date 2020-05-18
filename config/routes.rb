a5ea4db97359efb6fdaaa733087038015b18f758
Louis Mauriac <mauriaclouis@hotmail.com>
2020-05-18 18:43:49 +0200

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :plugs, only: [ :index, :new, :create, :update ] do 
+    resources :bookings, only: [ :new, :create ] 
+  end
+
+  resources :bookings, only: [:index, :show, :update] do
+    resources :messages, only: [ :index, :create]
+  end
 end
