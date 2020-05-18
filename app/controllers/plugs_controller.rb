5189019e03dee75f59b7d027ceae02088784d71b
phbasin <pierre-henri.basin@orange.fr>
2020-05-18 21:33:37 +0200

    index page added

--- app/controllers/plugs_controller.rb
+++ app/controllers/plugs_controller.rb
@@ -1,2 +1,24 @@
 class PlugsController < ApplicationController
+  def index
+    @plugs = Plug.all
+  end
+ 
+  def new
+    @plug = Plug.new
+  end
+ 
+  def create
+    @plug = Plug.new(plug_params)
+    @plug.save
+  end
+ 
+  def show
+    @plug = Plug.find(params[:id])
+  end
+ 
+  private
+ 
+  def plug_params
+    params.require(:plug).permit(:address, :power, :ad_dc, :type_plug, :price)
+  end
 end
