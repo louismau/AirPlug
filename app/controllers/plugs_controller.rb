4fd7246ceac0265711c5ee7bec8e796af4f5b95e
JB Dubois <jeanbaptiste@re-ve.fr>
2020-05-18 23:30:45 +0200

    user story almost done

--- app/controllers/plugs_controller.rb
+++ app/controllers/plugs_controller.rb
@@ -4,5 +4,21 @@ class PlugsController < ApplicationController
     @plug = Plug.new
   end
 
+  def create
+    @plug = Plug.new(require_params)
 
+    if @plug.save
+      redirect_to #
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def require_params
+    params.require(:plug).permit(:address, :price, :power, :ac_dc, :type_plug, photos: [])
+  end
+ 
 end
--- app/models/plug.rb
+++ app/models/plug.rb
@@ -2,5 +2,11 @@ class Plug < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many_attached :photos
-  
+
+  validates :address, presence: true
+  validates :price, presence: true
+  validates :ac_dc, presence: true
+  validates :type_plug, presence: true
+  validates :power, presence: true
+
 end
