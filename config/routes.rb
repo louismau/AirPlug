a6b4cd3ae8f93647a08cc90b1c43c3dfc95ccc15
JB Dubois <jeanbaptiste@re-ve.fr>
2020-05-19 16:45:16 +0200

    user controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  resources :users, only: [ :show, :edit, :update ]
+
   resources :plugs, only: [ :index, :new, :create, :update ] do 
     resources :bookings, only: [ :new, :create ] 
   end
