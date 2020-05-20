77db4e9e279d9c003f28cffb9ce0a797b59f63e7
Louis Mauriac <mauriaclouis@hotmail.com>
2020-05-20 12:06:44 +0200

    add map to index

---
+++ app/javascript/plugins/init_mapbox.js
@@ -0,0 +1,36 @@
+import mapboxgl from 'mapbox-gl';
+
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+};
+
+const addMarkersToMap = (map, markers) => {
+  markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
+
+    new mapboxgl.Marker()
+      .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup) // add this
+      .addTo(map);
+  });
+};
+
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
+
+  if (mapElement) { 
+    mapboxgl.accessToken = 'pk.eyJ1IjoibG91aXNtYXUiLCJhIjoiY2s5bzMzMGxhMDd3aTNubzh6amNqd3lsbCJ9.xuiUg1SNmZwvfFy-WRnYNQ';
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v10'
+    });
+    
+    const markers = JSON.parse(mapElement.dataset.markers);
+    fitMapToMarkers(map, markers);
+    addMarkersToMap(map, markers);
+  }
+};
+
+export { initMapbox };
