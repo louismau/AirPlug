1e032e391cbc2800e7777904d2d7a6d676115edd
Louis Mauriac <mauriaclouis@hotmail.com>
2020-05-20 16:21:06 +0200

    removed geoloc

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -21,19 +21,14 @@ const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
   if (mapElement) { 
-    navigator.geolocation.getCurrentPosition((data) => {
       mapboxgl.accessToken = 'pk.eyJ1IjoibG91aXNtYXUiLCJhIjoiY2s5bzMzMGxhMDd3aTNubzh6amNqd3lsbCJ9.xuiUg1SNmZwvfFy-WRnYNQ';
       const map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v10',
-        // center: [data.coords.latitude, data.coords.longitude],
-       // zoom: 12
       });
-      // map.setCenter([data.coords.latitude, data.coords.longitude])
       const markers = JSON.parse(mapElement.dataset.markers);
       fitMapToMarkers(map, markers);
       addMarkersToMap(map, markers);
-    });
   }
 };
 
